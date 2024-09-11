a92bba3823db0e4b89a91c797510ab9e7402580d
alexsh882 <alexsh882@gmail.com>
2024-09-11 11:22:27 -0300

    refactor: Agregar autenticación a las rutas de tipos de equipo y equipos

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import { AuthRoutes } from "./auth.routes";
 import { EquipmentTypeRoutes } from "./equipment-type.routes";
 import { EquipmentRoutes } from "./equipment.routes";
+import { isAuthenticated } from "../middlewares/authentication";
 
 export class AppRouter {
   static get routes(): Router {
     const router = Router();
 
     router.use("/api", AuthRoutes.routes);
-    router.use("/api", EquipmentTypeRoutes.routes);
-    router.use("/api", EquipmentRoutes.routes);
+    router.use("/api", isAuthenticated, EquipmentTypeRoutes.routes);
+    router.use("/api", isAuthenticated, EquipmentRoutes.routes);
 
     return router;
   }
