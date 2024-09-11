2e6de45d54ff8651b79e70904be2a27da22c622f
alexsh_home <alexsh882@gmail.com>
2024-09-10 21:34:06 -0300

    feat: Agregar servicio para obtener usuario por ID
    Se agregó un nuevo archivo `user.service.ts` que contiene la clase `UserService`. Esta clase tiene un método `getUserById` que permite obtener un usuario por su ID. El método utiliza el modelo `User` para buscar el usuario en la base de datos.

---
+++ src/features/users/user.service.ts
@@ -0,0 +1,9 @@
+import User from "../../models/users.model";
+
+export class UserService{
+    constructor(private userModel: typeof User){}
+
+    async getUserById(user_id: string){
+        return await this.userModel.findByPk(user_id);
+    }
+}
